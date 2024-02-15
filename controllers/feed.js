5f8397c3ec3e79c278c3368b7c639d62501caec7
Tanzeel <tanzeel498@gmail.com>
2024-02-15 13:48:02 +0500

    add validation to createPost route

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,42 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
-      { title: "blog1", content: "this is the content of blog 1." },
-      { title: "blog2", content: "this content belongs to the blog 2" },
+      {
+        _id: "kl34323",
+        title: "blog1",
+        creator: { name: "Tanzeel" },
+        imageUrl: "images/duck.jpg",
+        createdAt: new Date(),
+        content: "this is the content of blog 1.",
+      },
     ],
+    totalItems: 1,
   });
 };
 
 exports.createPost = (req, res) => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "Validation Failed, entered data is Incorrect!",
+        errors: result.array(),
+      });
+  }
   const { title, content } = req.body;
   // store post in the DB
 
   res.status(201).json({
     message: "Post created successfully",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: { name: "Tanzeel Khan" },
+      createdAt: new Date(),
+    },
   });
 };

a4c76582d7a974a75eaa0c82bebaaa4bfc2bf420
Tanzeel <tanzeel498@gmail.com>
2024-02-21 14:27:56 +0500

    fix socket.io connection and make socket.js to provide io accross the app

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const multer = require("multer");
 const { createServer } = require("node:http");
-const { Server } = require("socket.io");
-const cors = require("cors");
 
 const feedRoutes = require("./routes/feed");
 const authRoutes = require("./routes/auth");
 
 const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const httpServer = createServer(app);
+const io = require("./socket").init(httpServer);
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -37,20 +35,24 @@ const fileFilter = (req, file, cb) => {
   cb(null, false);
 };
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
+
 // below controller is to allow cors
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected!");
+});
 
 app.use("/auth", authRoutes);
 app.use(
@@ -58,7 +60,6 @@ app.use(
   multer({ storage: fileStorage, fileFilter }).single("image"),
   feedRoutes
 );
-
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
@@ -66,14 +67,10 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message, data });
 });
 
-io.on("connection", (socket) => {
-  console.log("A user connected!");
-});
-
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Mongoose Connected!");
-    server.listen(8080);
+    httpServer.listen(8080);
   })
   .catch((err) => console.error(err));
